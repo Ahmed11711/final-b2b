56f1ffb5dce91a18abf6dab905b0ef759b9b9fbf
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-08-26 11:29:42 +0300

    E.m signature ;)

--- src/service/services/apiService.js
+++ src/service/services/apiService.js
@@ -1,7 +1,8 @@
 // src/services/apiService.js
 import axios from "axios";
 
-const BASE_URL = "https://b2bpartnership.com/api/api/admin/v1/";
+// const BASE_URL = "https://b2bpartnership.com/api/api/admin/v1/";
+ const BASE_URL = "http://localhost:8000/api/admin/v1/"; // Use this for local development
 
 const api = axios.create({
   baseURL: BASE_URL,
